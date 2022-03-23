20a701539c102a23de82d50272b9ec9a47386dc2
bulatello28 <102026443+bulatello28@users.noreply.github.com>
2022-03-24 00:46:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo foo = new Foo();
+            foo.OnKeyPressed += (_, c) => Console.WriteLine($"\n{c}");
+
+            foo.Run();
+        }
+    }
+
+    public class Foo
+    {
+        public EventHandler<char> OnKeyPressed;
+        public void Run()
+        {
+            char input = ' ';
+
+            while (input != 'c')
+            {
+                input = Console.ReadKey().KeyChar;
+                OnKeyPressed.Invoke(this, input);
+            }
+        }
+    }
+}
